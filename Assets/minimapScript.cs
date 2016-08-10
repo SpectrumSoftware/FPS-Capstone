9d88fcfc59796e375382181e8b991ffa1734d867
SpectrumSoftware <spectrum.software.us@gmail.com>
2016-08-10 13:24:53 -0600

    Level bug fixes, Collider Changes, and minimap
    Fixed some collider issues to where the player could get above the map
    by running into the wall colliders, along with fixing collider issues
    with the tunnels... Also added a minimap that follows the player with an
    offset so its never in the player.

---
+++ Assets/minimapScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class minimapScript : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 followPlayer;
+    public int offset;
+	
+    void Update ()
+    {
+        followPlayer = new Vector3(player.transform.position.x, player.transform.position.y + offset, player.transform.position.z);
+        transform.position = followPlayer;
+    }
+
+}
